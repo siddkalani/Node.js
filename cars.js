2b91f8b836aa2dc31b302a350872d3e35539df3e
siddkalani <siddharthkalani6@gmail.com>
2024-06-17 02:09:37 +0530

    import export

---
+++ cars.js
@@ -0,0 +1,15 @@
+exports.car = {
+    brand:"skoda",
+    model:"octavia",
+}
+exports.ford = {
+    brand:"audi",
+    model:"s3",
+}
+
+// const car = {
+//     brand:"skoda",
+//     model:"octavia",
+// }
+//for exporting
+// module.exports = car;
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log("here is my first code")
 
+//for importing from another file
+const {car , ford} = require("./cars")
+// console.log(car)
+// console.log(ford)
 
+//for converting object to string
+console.log(JSON.stringify(car , null , 2))
+console.log(JSON.stringify(ford, null , 2))
