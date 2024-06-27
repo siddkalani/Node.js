115d990b4e5744901d86004d21d4ad30d3e27b95
siddkalani <siddharthkalani6@gmail.com>
2024-06-28 00:53:03 +0530

    first own api

---
+++ methods/get-request.js
@@ -0,0 +1,11 @@
+module.exports = (req , res) => {
+    if (req.url === "/api/movies") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type","application/json");
+        res.write(JSON.stringify(req.movies));
+        res.end();
+    } else {
+        res.writeHead(404 , {"Content-Type": "application/json"});
+        res.end( JSON.stringify({ message:"wrong" , title:"Not found" }))
+    }
+}
